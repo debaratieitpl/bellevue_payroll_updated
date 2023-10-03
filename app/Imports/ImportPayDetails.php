8f9f9ea0cd1749b9a900e717047f5a9e86356db3
aimssquad <abbasbitm3655@gmail.com>
2023-10-03 19:37:43 +0530

    loan-updated

--- app/Imports/ImportPayDetails.php
+++ app/Imports/ImportPayDetails.php
@@ -14,36 +14,22 @@ class ImportPayDetails implements ToModel,WithHeadingRow
     */
     public function model(array $row)
     {
-        dd($row);
+         //dd($row);
         //pf upate
         \DB::table('employee_pay_structures')
             ->where('employee_code', $row['emp_code'])
             ->update([
-            'basic_pay' => $row['basic'],
-            'apf_percent' => $row['apf'],
-            'da' => $row['da'],
-            'da_type' => $row['da_type'],
-            'tiff_alw' => $row['tifa'],
-            'tiff_alw_type' => $row['tifa_type'],
-            'conv' => $row['cva'],
-            'conv_type' => $row['cva_type'],
-            'others_alw' => $row['otha'],
-            'others_alw_type' => $row['otha_type'],
-            'misc_alw' => $row['misc1'],
-            'misc_alw_type' => $row['misc1_type'],
-            'medical' => $row['mdalw'],
-            'medical_type' => $row['mdalw_type'],
+            'pf' => $row['pf'],
+            'pf_type' => $row['pf_type'],
+            'apf' => $row['apf'],
+            'apf_type' => $row['apf_type'],
             'i_tax' => $row['itax'],
-            'i_tax_type' => $row['itax_type'],
-            'insu_prem' => $row['insp'],
-            'insu_prem_type' => $row['insp_type'],
-            'co_op' => $row['cops'],
-            'co_op_type' => $row['cops_type'],
-            'furniture' => $row['furn'],
-            'furniture_type' => $row['furn_type'],
+            'prof_tax' => $row['pftx'],
+            'prof_tax_type' => $row['prof_tax_type'],
         ]);
 
         //dd($row);
+        //Paystructure update
         // $emp_pf_inactuals = $row['basic'];
         // if ($emp_pf_inactuals > 15000) {
         //     $emp_pf_inactuals ='Y';
--- app/Imports/MonthlyEmployeeAttendanceImport.php
+++ app/Imports/MonthlyEmployeeAttendanceImport.php
@@ -10,7 +10,7 @@ class MonthlyEmployeeAttendanceImport implements WithHeadingRow, ToModel
 {
     public function model(array $row)
     {
-        //dd($row);
+        //dd($row['month']);
         \DB::table('process_attendances')
         ->where('employee_code', $row['employee_id'])
         ->where('month_yr', $row['month'])
@@ -21,5 +21,16 @@ class MonthlyEmployeeAttendanceImport implements WithHeadingRow, ToModel
             'no_of_days_salary' => $row['no_of_salary_days'],
             'no_sal_adjust_days' => $row['no_of_salary_adjustment_days'],
         ]);
+
+        // \DB::table('process_attendances')
+        // ->where('month_yr', $row['month']) // Filter by the current month
+        // ->whereNotIn('employee_code', $row['employee_id']) // Exclude employees in the update list
+        // ->update([
+        //     'no_of_present' => 0,
+        //     'no_of_days_leave_taken' => 0,
+        //     'no_of_days_absent' => 0,
+        //     'no_of_days_salary' => 0,
+        //     'no_sal_adjust_days' => 0,
+        // ]);
     }
 }
